a8bd9a5f1683b34ed11c012c09d986c73fe45413
Cole <49996960+CWoulf@users.noreply.github.com>
2021-07-30 14:23:50 -0500

    added ladder tilemap and ladder funciton

--- TileVania 2D Platformer/Assets/Scripts/Player.cs
+++ TileVania 2D Platformer/Assets/Scripts/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
     // Config (Stuff set before we start)
     [SerializeField] float runSpeed = 5f;
     [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f;
 
     // State
     bool isAlive = true;
@@ -15,12 +16,16 @@ public class Player : MonoBehaviour
     // Cached Component References
     Rigidbody2D rb;
     Animator anim;
+    Collider2D myCollider2D;
+    float gravityScaleAtStart;
 
     //Messages then methods
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        myCollider2D = GetComponent<Collider2D>();
+        gravityScaleAtStart = rb.gravityScale;
     }
 
     void Update()
@@ -28,12 +33,13 @@ public class Player : MonoBehaviour
         Run();
         FlipSprite();
         Jump();
+        ClimbLadder();
     }
 
     private void Run()
     {
-        float controlThrow = Input.GetAxis("Horizontal"); // value is between -1 to +1
-        Vector2 playerVel = new Vector2(controlThrow * runSpeed, rb.velocity.y);
+        float movement = Input.GetAxis("Horizontal"); // value is between -1 to +1
+        Vector2 playerVel = new Vector2(movement * runSpeed, rb.velocity.y);
         rb.velocity = playerVel;
 
         bool playerHasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
@@ -41,12 +47,32 @@ public class Player : MonoBehaviour
     }
     private void Jump()
     {
-        if(Input.GetButtonDown("Jump"))
+        if(!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))){ return; }
+        
+        if (Input.GetButtonDown("Jump"))
         {
             Vector2 jumpVelToAdd = new Vector2(0f, jumpSpeed);
             rb.velocity += jumpVelToAdd;
         }
     }
+    private void ClimbLadder()
+    {
+        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing"))) 
+        {
+            anim.SetBool("isClimbing", false);
+            rb.gravityScale = gravityScaleAtStart;
+            return; 
+        }
+
+        float climbing = Input.GetAxis("Vertical"); // value is between -1 to +1
+        Vector2 climbVel = new Vector2(rb.velocity.x, climbing * climbSpeed);
+        rb.velocity = climbVel;
+        rb.gravityScale = 0;
+
+        bool playerHasVerticalSpeed = Mathf.Abs(rb.velocity.y) > Mathf.Epsilon;
+        anim.SetBool("isClimbing", playerHasVerticalSpeed);
+        
+    }    
     private void FlipSprite()
     {
         bool playerHasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
