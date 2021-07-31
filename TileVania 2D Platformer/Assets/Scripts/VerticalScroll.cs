258a696a4dc7f10b733557bbc5493cc0cbe385a8
Cole <49996960+CWoulf@users.noreply.github.com>
2021-07-31 14:17:06 -0500

    made water rising level and tweaked all levels to work

---
+++ TileVania 2D Platformer/Assets/Scripts/VerticalScroll.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalScroll : MonoBehaviour
+{
+    [Tooltip("Game Units Per Second")]
+    [SerializeField] float scrollRate = 1f;
+
+    void Update()
+    {
+        float yMove = scrollRate * Time.deltaTime;
+        transform.Translate(new Vector2(0f, yMove));
+    }
+}
